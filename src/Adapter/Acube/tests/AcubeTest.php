9fc15689d43806f8d7c6c09572310e8bcabd863e
Daniele Rosario <daniele@weble.it>
2022-01-31 18:37:04 +0100

    acube tests with credentials

--- src/Adapter/Acube/tests/AcubeTest.php
+++ src/Adapter/Acube/tests/AcubeTest.php
@@ -5,8 +5,6 @@ use FatturaElettronicaPhp\Sender\Exceptions\InvalidCredentialsException;
 use FatturaElettronicaPhp\Sender\Exceptions\InvalidEnvironmentException;
 use FatturaElettronicaPhp\Sender\Exceptions\RequestException;
 
-$shouldSkip = ! isset($TEST_AUTHS['acube']);
-
 /**
  * ./vendor/bin/pest --group=acube
  */
@@ -26,49 +24,52 @@ it('cannot send without valid environment', function () {
 })->group('acube')->throws(InvalidEnvironmentException::class);
 
 it('can send sample invoice', function () {
-    $sender = new AcubeAdapter([
-        'email' => $TEST_AUTHS['email'] ?? '',
-        'password' => $TEST_AUTHS['password'] ?? '',
-        'environment' => AcubeAdapter::ENV_SANDBOX,
-    ]);
+    $credentials = (array)$this->credentials->acube ?? null;
+    if (! $credentials) {
+        $this->markTestSkipped();
+    }
+
+    $sender = new AcubeAdapter(
+        $credentials
+    );
+
     $path_xml_example = __DIR__ . DIRECTORY_SEPARATOR . "samples/invoice_sample.xml";
     $xml = file_get_contents($path_xml_example);
-    $uid = $sender->send($xml);
-    expect($uid)->toBeString();
-})->group('acube')->skip($shouldSkip);
+    $result = $sender->send($xml);
+
+    expect($result->get("uuid"))->toBeString();
+})->group('acube');
 
 it('can send multiline invoice', function () {
-    $sender = new AcubeAdapter([
-        'email' => $TEST_AUTHS['email'] ?? '',
-        'password' => $TEST_AUTHS['password'] ?? '',
-        'environment' => AcubeAdapter::ENV_SANDBOX,
-    ]);
+    $credentials = (array)$this->credentials->acube ?? null;
+    if (! $credentials) {
+        $this->markTestSkipped();
+    }
+
+    $sender = new AcubeAdapter(
+        $credentials
+    );
     $path_xml_example = __DIR__ . DIRECTORY_SEPARATOR . "samples/invoice_sample_multiline.xml";
     $xml = file_get_contents($path_xml_example);
-    $uid = $sender->send($xml);
-    expect($uid)->toBeString();
-})->group('acube')->skip($shouldSkip);
+    $result = $sender->send($xml);
 
-it('can send  simplified invoice', function () {
-    $sender = new AcubeAdapter([
-        'email' => $TEST_AUTHS['email'] ?? '',
-        'password' => $TEST_AUTHS['password'] ?? '',
-        'environment' => AcubeAdapter::ENV_SANDBOX,
-    ]);
-    $path_xml_example = __DIR__ . DIRECTORY_SEPARATOR . "samples/invoice_simplified_sample.xml";
-    $xml = file_get_contents($path_xml_example);
-    $uuid = $sender->send($xml);
-})->throws(RequestException::class)->group('acube')->skip($shouldSkip);
+    expect($result->get("uuid"))->toBeString();
+})->group('acube');
 
 it('can send simplified invoice', function () {
-    $sender = new AcubeAdapter([
-        'email' => $TEST_AUTHS['email'] ?? '',
-        'password' => $TEST_AUTHS['password'] ?? '',
-        'environment' => AcubeAdapter::ENV_SANDBOX,
-        'is_simplified' => true,
-    ]);
+    $credentials = (array)$this->credentials->acube ?? null;
+    if (! $credentials) {
+        $this->markTestSkipped();
+    }
+
+    $sender = new AcubeAdapter(
+        $credentials + [
+            'simplified' => true,
+        ],
+    );
     $path_xml_example = __DIR__ . DIRECTORY_SEPARATOR . "samples/invoice_simplified_sample.xml";
     $xml = file_get_contents($path_xml_example);
-    $uuid = $sender->send($xml);
-    expect($uuid)->toBeString();
-})->group('acube')->skip($shouldSkip);
+    $result = $sender->send($xml);
+
+    expect($result->get("uuid"))->toBeString();
+})->group('acube');
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,22 @@
 <?php
 
-$TEST_AUTHS = [];
+$adaptersWithTestCredentials = [
+    'Acube',
+    'Aruba',
+    'EFFatta',
+];
 
-$filePath = dirname(__DIR__) . '/.auth.json';
-if (file_exists($filePath)) {
-    $TEST_AUTHS = json_decode(file_get_contents($filePath));
-}
+$adaptersWithTestCredentials = array_map(fn ($d) => dirname(__DIR__). "/src/Adapter/$d/tests/", $adaptersWithTestCredentials);
+
+uses()->beforeEach(function () {
+    $this->credentials = [];
+    $this->hasCredentials = false;
+
+    $filePath = dirname(__DIR__) . '/.auth.json';
+    if (file_exists($filePath)) {
+        $this->credentials = json_decode(file_get_contents($filePath));
+        $this->hasCredentials = true;
+    }
+
+
+})->in(...$adaptersWithTestCredentials);
